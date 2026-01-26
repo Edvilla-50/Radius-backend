a0bbab9d8784be4196daf9816dd9da883916e58a
hello-vro <69919972+Edvilla-50@users.noreply.github.com>
2026-01-26 15:33:18 -0700

    More code3 vro <3

--- src/main/java/com/Radius/backend/Forumulas/GoongaGingaAlgorithm.java
+++ src/main/java/com/Radius/backend/Forumulas/GoongaGingaAlgorithm.java
@@ -1,9 +1,11 @@
 package com.Radius.backend.Forumulas;
+import org.hibernate.query.sqm.tree.expression.Compatibility;
+
 import com.Radius.backend.*;
 import com.Radius.backend.Entity.*;
 public class GoongaGingaAlgorithm {
     Haversine hs = new Haversine();
-    double distance = hs.haversine(one.getLat(),one.getLon(),two.getLat(),two.getLon());
-    
-
+    public User compatibility(User radius){
+        return radius;
+    }
 }
