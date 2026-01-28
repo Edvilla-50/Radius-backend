b99a8babb44c1ae48256c3391afd10a1132d4cb4
hello-vro <69919972+Edvilla-50@users.noreply.github.com>
2026-01-27 18:56:35 -0700

    Algorithm building part 2vro

--- src/main/java/com/Radius/backend/Forumulas/GoongaGingaAlgorithm.java
+++ src/main/java/com/Radius/backend/Forumulas/GoongaGingaAlgorithm.java
@@ -6,6 +6,11 @@ import com.Radius.backend.Entity.*;
 public class GoongaGingaAlgorithm {
     Haversine hs = new Haversine();
     public User compatibility(User radius){
-        return radius;
+        double latDistance = radius.getPerferredDistance() / 69.0; // 1 degree lat ~ 69 miles, I hate math man
+        double lonDistance = radius.getPerferredDistance() / (69.0 * Math.cos(Math.toRadians(radius.getLat())));//same thing for lon
+        double maxLat = radius.getLat() + latDistance;
+        double minLat = radius.getLat() - latDistance;
+        double maxLon = radius.getLon() + lonDistance;
+        double minLon = radius.getLon() - lonDistance;
     }
 }
--- src/main/java/com/Radius/backend/Services/MatchService.java
+++ src/main/java/com/Radius/backend/Services/MatchService.java
@@ -8,18 +8,18 @@ import java.util.List;
 @Service
 public class MatchService {
     private final UserRepository repo;
-    private final GoongaGingaAlgorithm algo = new GoongaGingaAlgorithm();
+    private final GoongaGingaAlgorithm algo = new GoongaGingaAlgorithm();//GoongaGingaAlogirithm object
     public MatchService(UserRepository repo){
         this.repo = repo;
     }
     public List<User> findMatches(long myId){
         User me = repo.findById(myId).orElseThrow();
         List<User> all = repo.findAll();
-        return all.stream()
+        return all.stream()//JPA methods to filter
             .filter(u -> !u.getId().equals(myId))
             .sorted((a,b) ->
                 Double.compare(
-                    algo.compatibility(me,b),
+                    algo.compatibility(me,b),//methon in goonga ginga lgorithm
                     algo.compatibility(me,a)
                 )
             )
