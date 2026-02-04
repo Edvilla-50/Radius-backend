1a84a6e64f50d4647eef3500006196020d0b8571
hello-vro <69919972+Edvilla-50@users.noreply.github.com>
2026-02-04 14:58:58 -0700

    algorithm vro <3

--- src/main/java/com/Radius/backend/Bases/UserRepository.java
+++ src/main/java/com/Radius/backend/Bases/UserRepository.java
@@ -2,8 +2,9 @@ package com.Radius.backend.Bases;
 import com.Radius.backend.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    List<User> findByLatBetweenAndLonBetween(double minLat, double maxLat, double minLon, double maxLon);
 }
--- src/main/java/com/Radius/backend/Forumulas/GetInnocuous.java
+++ src/main/java/com/Radius/backend/Forumulas/GetInnocuous.java
@@ -1,6 +1,7 @@
 package com.Radius.backend.Forumulas;
 import com.Radius.backend.Bases.UserRepository;
 import com.Radius.backend.Entity.User;
+import java.util.List;
 
 public class GetInnocuous {
 
@@ -18,10 +19,16 @@ public class GetInnocuous {
         double lat = me.getLat();
         double pref = me.getPerferredDistance();
         double deltaLat = pref/69.00;
-        double deltaLon = pref/(69*Math.cos(lat));
+        double deltaLon = pref / (69 * Math.cos(Math.toRadians(lat)));
         double MaxNorth = lat +deltaLat;
         double MaxEast = lon+deltaLon;
         double maxWest = lon-deltaLon;
-        double maxSouth = lon-deltaLat;
+        double maxSouth = lat-deltaLat;
+        double distanceNorth = hs.haversine(lat, lon, MaxNorth, lon);
+        double distanceEast  = hs.haversine(lat, lon, lat, MaxEast);
+        double distanceWest = hs.haversine(lat, lon, lat, maxWest);
+        double distanceSouth = hs.haversine(lat, lon, maxSouth, lon);
+        List<User> canidates = RepoSearch.findByLatBetweenAndLonBetween(distanceSouth,distanceNorth, distanceWest,distanceEast);
+
     }
 }
