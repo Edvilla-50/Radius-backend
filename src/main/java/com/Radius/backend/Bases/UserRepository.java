034f6ac0f264a5fc3dff8d8843538a1f52dc60d0
hello-vro <69919972+Edvilla-50@users.noreply.github.com>
2026-02-09 14:01:50 -0700

    We got the list of nearby users vro <3

--- src/main/java/com/Radius/backend/Bases/UserRepository.java
+++ src/main/java/com/Radius/backend/Bases/UserRepository.java
@@ -6,5 +6,10 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByLatBetweenAndLonBetween(double minLat, double maxLat, double minLon, double maxLon);
+    List<User> findByLatBetweenAndLonBetween(
+        double minLat,
+        double maxLat,
+        double minLon,
+        double maxLon
+    );
 }
--- src/main/java/com/Radius/backend/Forumulas/GetInnocuous.java
+++ src/main/java/com/Radius/backend/Forumulas/GetInnocuous.java
@@ -15,20 +15,16 @@ public class GetInnocuous {
      * Higher score = more compatible
      */
     public float compatibility(User me) {
-        double lon = me.getLon();
-        double lat = me.getLat();
-        double pref = me.getPerferredDistance();
+        double lon = me.getLon();//get user current longitude
+        double lat = me.getLat();//get user current latitude
+        double pref = me.getPerferredDistance();//
         double deltaLat = pref/69.00;
         double deltaLon = pref / (69 * Math.cos(Math.toRadians(lat)));
         double MaxNorth = lat +deltaLat;
         double MaxEast = lon+deltaLon;
-        double maxWest = lon-deltaLon;
-        double maxSouth = lat-deltaLat;
-        double distanceNorth = hs.haversine(lat, lon, MaxNorth, lon);
-        double distanceEast  = hs.haversine(lat, lon, lat, MaxEast);
-        double distanceWest = hs.haversine(lat, lon, lat, maxWest);
-        double distanceSouth = hs.haversine(lat, lon, maxSouth, lon);
-        List<User> canidates = RepoSearch.findByLatBetweenAndLonBetween(distanceSouth,distanceNorth, distanceWest,distanceEast);
-
+        double MaxWest = lon-deltaLon;
+        double MaxSouth = lat-deltaLat;
+        List<User> nearby = RepoSearch.findByLatBetweenAndLonBetween(MaxSouth,MaxNorth,MaxWest,MaxEast);
+        return 0.0f;
     }
 }
