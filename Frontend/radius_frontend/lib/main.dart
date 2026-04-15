ee3c2e6ae2be12323699905599e25725dfb4b13b
hello-vro <69919972+Edvilla-50@users.noreply.github.com>
2026-04-15 15:07:57 -0600

    more frontend

--- Frontend/radius_frontend/lib/main.dart
+++ Frontend/radius_frontend/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
-import 'screens/MapScreen.dart';
+import 'screens/HomeScreen.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Radius',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor:  Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home:const MapScreen(userId:71),
+      home: const HomeScreen(), 
     );
   }
 }
