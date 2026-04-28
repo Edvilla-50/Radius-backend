4c17bda5e8725976151988fa32c7cafe02f86fa2
hello-vro <69919972+Edvilla-50@users.noreply.github.com>
2026-04-28 13:32:20 -0600

    almost done

--- src/main/java/com/Radius/backend/Aspects/Interests.java
+++ src/main/java/com/Radius/backend/Aspects/Interests.java
@@ -8,5 +8,5 @@ public interface Interests {
     public void setDisAccesible(boolean con2);//if the hobby is accisble to people of disabilty
     public void setMoneyCond(boolean con3);//does the hobby require moneh?
     public void setDifficulty(String diff, String state);//how hard if the hobby, in any sense (mentally, physically,etc) 
-    public void setMeetUpTime(String Time);//meetup time
+    public void setMeetUpTime(String Time);//meetup time]
 }
--- src/main/java/com/Radius/backend/Entity/InterestEntity.java
+++ src/main/java/com/Radius/backend/Entity/InterestEntity.java
@@ -1,9 +1,8 @@
 package com.Radius.backend.Entity;
-import com.Radius.backend.Aspects.*;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "interests")//Transition to acceptable JPA polymorhpism, well its called persistence I guess
+@Table(name = "interests")
 public class InterestEntity {
 
     @Id
@@ -12,6 +11,12 @@ public class InterestEntity {
 
     @Column(unique = true)
     private String name;
+
+    private Boolean moneyNeeded;      
+    private Boolean disAccessible;
+    private String difficulty;
+    private String state;
+    private String meetUpTime;
 
     public InterestEntity() {}
     public InterestEntity(String name) { 
@@ -27,5 +32,34 @@ public class InterestEntity {
     public void setName(String name) { 
         this.name = name; 
     }
+    public Boolean isMoneyNeeded() { 
+        return moneyNeeded; 
+    }
+    public void setMoneyNeeded(Boolean moneyNeeded) { 
+        this.moneyNeeded = moneyNeeded; 
+    }
+    public Boolean isDisAccessible() { 
+        return disAccessible; 
+    }
+    public void setDisAccessible(Boolean disAccessible) { 
+        this.disAccessible = disAccessible; 
+    }
+    public String getDifficulty() { 
+        return difficulty; 
+    }
+    public void setDifficulty(String difficulty) { 
+        this.difficulty = difficulty; 
+    }
+    public String getState() { 
+        return state; 
+    }
+    public void setState(String state) { 
+        this.state = state; 
+    }
+    public String getMeetUpTime() { 
+        return meetUpTime; 
+    }
+    public void setMeetUpTime(String meetUpTime) { 
+        this.meetUpTime = meetUpTime; 
+    }
 }
-
