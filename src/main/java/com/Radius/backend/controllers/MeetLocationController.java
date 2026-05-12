9851121a165e0748767aeb8c9b708cb3192e60dc
hello-vro <69919972+Edvilla-50@users.noreply.github.com>
2026-05-12 15:12:12 -0600

    missing endpoint

--- src/main/java/com/Radius/backend/controllers/MeetLocationController.java
+++ src/main/java/com/Radius/backend/controllers/MeetLocationController.java
@@ -1,7 +1,10 @@
 package com.Radius.backend.controllers;
 
 import com.Radius.backend.Entity.MeetLocation;
+import com.Radius.backend.Services.MatchService;
 import com.Radius.backend.Services.MeetLocationService;
+import com.Radius.backend.Services.MeetService;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -33,4 +36,24 @@ public class MeetLocationController {
     public MeetLocation getLocation(@PathVariable int matchId) {
         return service.getLocation(matchId);
     }
+    @RestController
+@RequestMapping("/match")
+@CrossOrigin(origins = "*")
+public class MatchController {
+
+    private final MatchService service;
+    private final MeetService meetService;
+
+    public MatchController(MatchService service, MeetService meetService) {
+        this.service = service;
+        this.meetService = meetService;
+    }
+
+
+    @GetMapping("/meet/suggestions/interests/{a}/{b}")
+    public Map<String, Object> getInterestSuggestions(@PathVariable int a, @PathVariable int b) {
+        return meetService.getSuggestionsForUsers(a, b);
+    }
+}
+
 }
