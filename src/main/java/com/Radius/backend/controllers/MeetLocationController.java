69dd4ba8b995db94272da1c5b996257c31b0ebe6
hello-vro <69919972+Edvilla-50@users.noreply.github.com>
2026-05-12 15:19:30 -0600

    duplicate mapping fix

--- src/main/java/com/Radius/backend/controllers/MeetLocationController.java
+++ src/main/java/com/Radius/backend/controllers/MeetLocationController.java
@@ -1,10 +1,7 @@
 package com.Radius.backend.controllers;
 
 import com.Radius.backend.Entity.MeetLocation;
-import com.Radius.backend.Services.MatchService;
 import com.Radius.backend.Services.MeetLocationService;
-import com.Radius.backend.Services.MeetService;
-
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -22,7 +19,6 @@ public class MeetLocationController {
 
     @PostMapping("/select-location")
     public MeetLocation selectLocation(@RequestBody Map<String, Object> body) {
-
         int matchId = (int) body.get("matchId");
         int userId = (int) body.get("userId");
         String locationId = (String) body.get("locationId");
@@ -36,24 +32,4 @@ public class MeetLocationController {
     public MeetLocation getLocation(@PathVariable int matchId) {
         return service.getLocation(matchId);
     }
-    @RestController
-@RequestMapping("/match")
-@CrossOrigin(origins = "*")
-public class MatchController {
-
-    private final MatchService service;
-    private final MeetService meetService;
-
-    public MatchController(MatchService service, MeetService meetService) {
-        this.service = service;
-        this.meetService = meetService;
-    }
-
-
-    @GetMapping("/meet/suggestions/interests/{a}/{b}")
-    public Map<String, Object> getInterestSuggestions(@PathVariable int a, @PathVariable int b) {
-        return meetService.getSuggestionsForUsers(a, b);
-    }
-}
-
 }
