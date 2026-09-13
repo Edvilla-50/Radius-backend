dc67de79105cdea5f1aecaeeb12103a2ec85bfcf
Edvilla-50 <eddieseashells@gmail.com>
2026-09-13 00:26:44 -0600

    cors fix

--- src/main/java/com/Radius/backend/configs/CorsConfig.java
+++ src/main/java/com/Radius/backend/configs/CorsConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -13,6 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                     .allowedOrigins(
+                        "https://www.radius-create.com",
                         "https://profile.radius-create.com",
                         "https://radius-create.com",
                         "http://localhost:3000",
